5385048917237653b3dc07b6eabeb1752531dee3
ShankarNara <shankar.sn007@gmail.com>
2019-09-11 12:14:03 +0530

    first commit

---
+++ app/src/main/java/com/example/drplip/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.drplip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.SessionsSettings;
+import com.google.cloud.dialogflow.v2.TextInput;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int USER = 10001;
+    private static final int BOT = 10002;
+
+    private String uuid = UUID.randomUUID().toString();
+    private LinearLayout chatLayout;
+    private EditText queryEditText;
+    Button sendBtn;
+
+    private SessionsClient sessionsClient;
+    private SessionName session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ScrollView scrollview = findViewById(R.id.chatScrollView);
+
+        chatLayout = findViewById(R.id.chatLayout);
+
+        sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+        queryEditText = findViewById(R.id.queryEditText);
+        //set an onclick listener
+    }
+
+    private void initV2Chatbot() {
+        try {
+            InputStream stream = getResources().openRawResource(R.raw.test_agent_credentials);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream);
+            String projectId = ((ServiceAccountCredentials)credentials).getProjectId();
+
+            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
+            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+            sessionsClient = SessionsClient.create(sessionsSettings);
+            session = SessionName.of(projectId, uuid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage() {
+        String msg = queryEditText.getText().toString();
+        if (msg.trim().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please enter your query!", Toast.LENGTH_LONG).show();
+        } else {
+            showTextView(msg, USER);
+            queryEditText.setText("");
+
+            // Java V2
+            QueryInput queryInput = QueryInput.newBuilder().setText(TextInput.newBuilder().setText(msg).setLanguageCode("en-US")).build();
+            new RequestJavaV2Task(MainActivity.this, session, sessionsClient, queryInput).execute();
+        }
+    }
+
+    private void showTextView(String message, int type) {
+        FrameLayout layout;
+        switch (type) {
+            case USER:
+                layout = getUserLayout();
+                break;
+            case BOT:
+                layout = getBotLayout();
+                break;
+            default:
+                layout = getBotLayout();
+                break;
+        }
+        layout.setFocusableInTouchMode(true);
+        chatLayout.addView(layout); // move focus to text view to automatically make it scroll up if softfocus
+        TextView tv = layout.findViewById(R.id.chatMsg);
+        tv.setText(message);
+        layout.requestFocus();
+        queryEditText.requestFocus(); // change focus back to edit text to continue typing
+    }
+
+    FrameLayout getUserLayout() {
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        return (FrameLayout) inflater.inflate(R.layout.user_msg_layout, null);
+    }
+
+    FrameLayout getBotLayout() {
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        return (FrameLayout) inflater.inflate(R.layout.bot_msg_layout, null);
+    }
+
+}
